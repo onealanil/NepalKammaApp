8860c151f9d6c34f40cdb6b1a7c5e8269c846e6c
Anil <khalifaanil84@gmail.com>
2025-04-26 23:12:02 +0545

    update config and json

--- src/global/config.js
+++ src/global/config.js
@@ -6,15 +6,21 @@
 
 import axios from 'axios';
 import {getTokenKeyChain} from '../utils/asyncStorage';
+import {REACT_APP_BACKEND_API_URL, REACT_APP_NODE_BACKEND_URL} from '@env';
 
 //college testing ip -> IIC staff
-export const API_URL = 'http://192.168.20.52:8000/api/v1';
+// export const API_URL = 'http://192.168.20.52:8000/api/v1';
+export const API_URL = 'http://192.168.1.18:8000/api/v1';
+// export const API_URL = REACT_APP_BACKEND_API_URL;
+// console.log('API_URL:', API_URL);s
 
 // export const API_URL = 'http://192.168.1.18:8000/api/v1';
 
 //college
-export const BACKEND_URL = 'http://192.168.20.52:8000';
-// export const BACKEND_URL = 'http://192.168.1.18:8000';
+// export const BACKEND_URL = 'http://192.168.20.52:8000';
+// export const BACKEND_URL = REACT_APP_NODE_BACKEND_URL;
+export const BACKEND_URL = 'http://192.168.1.18:8000';
+// console.log('BACKEND_URL:', BACKEND_URL);
 
 // Axios instance without authentication
 export const axios_no_auth = axios.create({
